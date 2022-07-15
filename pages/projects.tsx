5c5b812b84f7d8ffbf8bc4af67dc3d5544fa0eb7
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-07-15 21:42:50 +0100

    Changed to use the PageTitle component

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
+
 import Navbar from './components/Navbar'
+import PageTitle from './components/PageTitle'
 
 export default function projects() {
     return (
         <div className="bg-purple min-h-screen">
             <Navbar />
 
-            <div className="flex justify-center mt-3">
-                <h1 className="text-2xl text-white">Personal Projects</h1>
-            </div>
+            <PageTitle title="Personal Projects" />
         </div>
     )
 }
