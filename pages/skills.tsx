39475270e1f2100e6a649413073f16910637145a
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-07-15 21:43:14 +0100

    Added a bare Skills page

---
+++ pages/skills.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Navbar from './components/Navbar'
+import PageTitle from './components/PageTitle'
+
+export default function skills() {
+  return (
+    <div className="bg-purple min-h-screen">
+        <Navbar />
+        <PageTitle title="My Skills" />
+
+    </div>
+  )
+}
