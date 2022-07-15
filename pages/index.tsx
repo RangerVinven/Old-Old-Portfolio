7e1fd80e6b4661590fd52b7d6505989f39abd997
RangerVinven <danielmcpherson1195@gmail.com>
2022-07-15 16:34:31 +0100

    Added the particles animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,21 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
 
+import Navbar from './components/Navbar'
 import ParticlesComponent from './components/ParticlesComponent'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Daniel McPherson</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-	  <ParticlesComponent />
-      <div className="flex justify-center items-center flex-col w-full h-full">
+    <div>
+		<Navbar />
 		
-		<h1 className="text-6xl font-bold text-white mb-2">Hi, I'm Daniel McPherson.</h1>
-		<h1 className="text-5xl font-semibold text-white">I'm a Fullstack Developer</h1>
-      </div>
-        
+		<div className="flex min-h-screen flex-col items-center justify-center py-2">
+			<ParticlesComponent />
+			
+			<div className="flex justify-center items-center flex-col w-full h-full">
+				<h1 className="text-6xl font-bold text-white mb-2">Hi, I'm Daniel McPherson.</h1>
+				<h1 className="text-5xl font-semibold text-white">I'm a Fullstack Developer</h1>
+			</div>
+		</div>
     </div>
   )
 }
