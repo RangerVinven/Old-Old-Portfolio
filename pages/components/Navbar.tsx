be36e68e81b8e7d6284e7f5566aefaf510e447e9
RangerVinven <danielmcpherson1195@gmail.com>
2022-07-15 16:06:25 +0100

    Added cursor-pointer and router redirect

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import Router from 'next/router';
+
 export default function Navbar() {
 	return (
 		<div className="flex w-full">
@@ -6,11 +8,11 @@ export default function Navbar() {
 			</div>
 			<div className="flex justify-end w-full">
 				<div className="flex w-1/4 justify-around">
-					<a href="#" className="text-lg text-white font-semibold">Home</a>
-					<a href="#" className="text-lg text-white font-semibold">Projects</a>
-					<a href="#" className="text-lg text-white font-semibold">Experience</a>
-					<a href="#" className="text-lg text-white font-semibold">Skills</a>
-					<a href="#" className="text-lg text-white font-semibold">Contact</a>
+					<h3 onClick={() => Router.push("/")} className="text-lg text-white font-semibold hover:cursor-pointer">Home</h3>
+					<h3 onClick={() => Router.push("/projects")} className="text-lg text-white font-semibold hover:cursor-pointer">Projects</h3>
+					<h3 onClick={() => Router.push("/experience")} className="text-lg text-white font-semibold hover:cursor-pointer">Experience</h3>
+					<h3 onClick={() => Router.push("/skills")} className="text-lg text-white font-semibold hover:cursor-pointer">Skills</h3>
+					<h3 onClick={() => Router.push("/contact")} className="text-lg text-white font-semibold hover:cursor-pointer">Contact</h3>
 				</div>
 			</div>
 		</div>
