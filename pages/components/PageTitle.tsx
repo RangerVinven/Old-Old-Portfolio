0db0138577019f32956df0915e229fd3a6e3c05e
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-07-16 01:11:21 +0100

    Increased size and made bold

--- pages/components/PageTitle.tsx
+++ pages/components/PageTitle.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function PageTitle(props: Props) {
     return (
         <div className="flex justify-center mt-3">
-            <h1 className="text-2xl text-white">{props.title}</h1>
+            <h1 className="text-3xl text-white font-bold">{props.title}</h1>
         </div>
     )
 }
