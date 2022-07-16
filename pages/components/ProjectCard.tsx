084bb794c08447e35a7145c3b7953cfc91dd6ed0
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-07-16 15:15:56 +0100

    Added props and added a title, description and area where the image should go

--- pages/components/ProjectCard.tsx
+++ pages/components/ProjectCard.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
-export default function ProjectCard() {
+type Props = {
+    title: String,
+    description: String
+}
+
+export default function ProjectCard(props: Props) {
   return (
-    <div>ProjectCard</div>
+    <div className="h-full w-1/4 bg-white rounded-lg shadow-xl">
+        <div className="bg-gray-400 w-full h-1/3 rounded-t-md"></div>
+    
+        <div className="ml-1">
+            <h1 className="text-purple font-bold text-xl">{props.title}</h1>
+            <p>
+                {props.description}
+            </p>
+        </div>
+    </div>
   )
 }
