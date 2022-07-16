3d70150129b607b0516149a588229b601b82fa66
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-07-16 17:30:50 +0100

    Added projectInfo.ts

---
+++ projectInfo.ts
@@ -0,0 +1,13 @@
+export const portfolio = {
+    imageAddress: "/images/portfolio.jpg",
+    title: "My Portfolio",
+    description: "The very website that you're on just now. I made this website to show my skills and to play around with various NPM packages",
+    githubLink: "https://github.com/RangerVinven/Portfolio",
+}
+
+export const funFarms = {
+    imageAddress: "/images/funFarms.jpg",
+    title: "Fun Farms",
+    description: "During my Software Development apprenticeship, we had a section on Website Design & Development. For our final assignment, we were tasked with creating a website in plain HTML and CSS that advertises a farm with various facilities such as a cafe, petting zoo, etc. This is what I made.",
+    githubLink: "https://github.com/RangerVinven/Fun-Farms",
+}
