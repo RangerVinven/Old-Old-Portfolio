e8fd3339c3e5620c10def143508f870f20370f86
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-07-16 16:16:21 +0100

    Added a githubLink to the portfolio

--- pages/components/ProjectCard.tsx
+++ pages/components/ProjectCard.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 
 type Props = {
-    title: String,
-    description: String
+    title: string,
+    description: string,
+    githubLink: string
 }
 
 export default function ProjectCard(props: Props) {
   return (
     <div className="h-full bg-white rounded-lg shadow-xl">
-        <img src="/images/portfolioScreenshot.png" alt="Portfolio" className="w-full h-1/3" />
+    	<a href={props.githubLink}>
+			<img src="/images/portfolioScreenshot.png" alt="Portfolio" className="w-full h-1/3" />
     
-        <div className="ml-1">
-            <h1 className="text-purple font-bold text-xl select-none">{props.title}</h1>
-            <p className="select-none">
-                {props.description}
-            </p>
-        </div>
+			<div className="ml-1">
+				<h1 className="text-purple font-bold text-xl select-none">{props.title}</h1>
+				<p className="select-none">
+					{props.description}
+				</p>
+			</div>
+		</a>
     </div>
   )
 }
