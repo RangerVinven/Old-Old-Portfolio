11430d9c04fe5672d131f445d938c67e48fcd442
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-07-16 01:08:06 +0100

    Moved the category styling to a categoryClass variable

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -11,6 +11,8 @@ export default function skills() {
   // Which data to show in the bar chart
   const [selectedCategory, setselectedCategory] = useState(Categories.Languages);
 
+  const categoryClass = "text-lg font-bold mb-2.5 hover:cursor-pointer"
+
   return (
     <div className="bg-purple h-screen overflow-hidden">
         <Navbar />
@@ -18,11 +20,11 @@ export default function skills() {
 
         <div className="flex justify-center h-full w-full mt-28">
             <div className="flex flex-col justify-center items-center bg-white h-fit w-fit px-2 py-1 rounded-lg mr-3.5">
-                <h3 className="text-lg font-bold mb-2.5 hover:cursor-pointer" onClick={() => setselectedCategory(Categories.Languages)}>Languages</h3>
-                <h3 className="text-lg font-bold mb-2.5 hover:cursor-pointer" onClick={() => setselectedCategory(Categories.Frameworks)}>Frameworks</h3>
-                <h3 className="text-lg font-bold mb-2.5 hover:cursor-pointer" onClick={() => setselectedCategory(Categories.Methodologies)}>Methodologies</h3>
-                <h3 className="text-lg font-bold mb-2.5 hover:cursor-pointer" onClick={() => setselectedCategory(Categories.Databases)}>Databases</h3>
-                <h3 className="text-lg font-bold mb-1.5 hover:cursor-pointer" onClick={() => setselectedCategory(Categories.DevOps)}>DevOps</h3>
+                <h3 className={categoryClass} onClick={() => setselectedCategory(Categories.Languages)}>Languages</h3>
+                <h3 className={categoryClass} onClick={() => setselectedCategory(Categories.Methodologies)}>Methodologies</h3>
+                <h3 className={categoryClass} onClick={() => setselectedCategory(Categories.Frameworks)}>Frameworks</h3>
+                <h3 className={categoryClass} onClick={() => setselectedCategory(Categories.Databases)}>Databases</h3>
+                <h3 className={categoryClass} onClick={() => setselectedCategory(Categories.DevOps)}>DevOps</h3>
             </div>
 
             <div className="bg-white h-3/6 w-2/5 px-2 py-1 rounded-lg">
