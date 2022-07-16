fff913b6697e659d89662c20dac6c16fce9dcb91
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-07-16 14:28:57 +0100

    Added a select-none attribute to the body text

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,8 +12,8 @@ const Home: NextPage = () => {
 			<ParticlesComponent />
 			
 			<div className="flex justify-center items-center flex-col w-full h-full">
-				<h1 className="text-6xl font-bold text-white mb-2">Hi, I'm Daniel McPherson.</h1>
-				<h1 className="text-5xl font-semibold text-white mb-2">I'm a Software Development student.</h1>
+				<h1 className="text-6xl font-bold text-white mb-2 select-none">Hi, I'm Daniel McPherson.</h1>
+				<h1 className="text-5xl font-semibold text-white mb-2 select-none">I'm a Software Development student.</h1>
 				<a href="https://github.com/RangerVinven" target="_blank" rel="noopener noreferrer"><img src="/githubIcon.png" alt="Github icon" width={32} height={32} /></a>
 			</div>
 		</div>
