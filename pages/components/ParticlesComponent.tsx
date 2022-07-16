ae1650aec5ac51ff0199646853d4bc709042f99f
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-07-16 14:29:55 +0100

    Removed the particlesLoaded function

--- pages/components/ParticlesComponent.tsx
+++ pages/components/ParticlesComponent.tsx
@@ -4,16 +4,10 @@ import { loadFull } from "tsparticles";
 export default function ParticlesComponent() {
 
 	const particlesInit = async (main: any) => {
-		console.log(main);
-
 		// you can initialize the tsParticles instance (main) here, adding custom shapes or presets
 		// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
 		// starting from v2 you can add only the features you need reducing the bundle size
 		await loadFull(main);
-	};
-
-	const particlesLoaded = (container: any) => {
-		console.log(container);
 	};
 
 	return (
