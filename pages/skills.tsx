0c093933b72603f16880b7c0c3a36215f6d0857b
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-07-16 01:03:35 +0100

    Made the data shown dynamic based on the selected category

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -1,30 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-import Navbar from './components/Navbar'
-import PageTitle from './components/PageTitle'
-import BarChart from './components/BarChart'
+import Navbar from './components/Navbar';
+import PageTitle from './components/PageTitle';
+import BarChart from './components/BarChart';
+
+import {Categories} from './components/BarChart';
 
 export default function skills() {
+
+  // Which data to show in the bar chart
+  const [selectedCategory, setselectedCategory] = useState(Categories.Languages);
+
   return (
     <div className="bg-purple h-screen overflow-hidden">
         <Navbar />
         <PageTitle title="My Skills" />
 
-        <br />
-        <br />
-        <br />
-
-        <div className="flex justify-center h-full w-fulbg-red-500">
+        <div className="flex justify-center h-full w-fulbg-red-500 mt-28">
             <div className="bg-white h-3/6 w-fit px-2 py-1 rounded-lg mr-3.5">
-                <h3 className="text-lg font-bold mb-2.5 hover:cursor-pointer">Languages</h3>
-                <h3 className="text-lg font-bold mb-2.5 hover:cursor-pointer">Frameworks</h3>
-                <h3 className="text-lg font-bold mb-2.5 hover:cursor-pointer">Methodologies</h3>
-                <h3 className="text-lg font-bold mb-2.5 hover:cursor-pointer">Databases</h3>
-                <h3 className="text-lg font-bold mb-1.5 hover:cursor-pointer">DevOps</h3>
+                <h3 className="text-lg font-bold mb-2.5 hover:cursor-pointer" onClick={() => setselectedCategory(Categories.Languages)}>Languages</h3>
+                <h3 className="text-lg font-bold mb-2.5 hover:cursor-pointer" onClick={() => setselectedCategory(Categories.Frameworks)}>Frameworks</h3>
+                <h3 className="text-lg font-bold mb-2.5 hover:cursor-pointer" onClick={() => setselectedCategory(Categories.Methodologies)}>Methodologies</h3>
+                <h3 className="text-lg font-bold mb-2.5 hover:cursor-pointer" onClick={() => setselectedCategory(Categories.Databases)}>Databases</h3>
+                <h3 className="text-lg font-bold mb-1.5 hover:cursor-pointer" onClick={() => setselectedCategory(Categories.DevOps)}>DevOps</h3>
             </div>
 
             <div className="bg-white h-3/6 w-2/5 px-2 py-1 rounded-lg">
-                <BarChart />
+                <BarChart dataToShow={selectedCategory} />
             </div>
 
         </div>
