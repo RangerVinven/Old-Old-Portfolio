b6bfbb9eda8465a2cf4996dfa5f01acfd0a3ae95
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-07-16 15:36:53 +0100

    Added select-none to the title and description

--- pages/components/ProjectCard.tsx
+++ pages/components/ProjectCard.tsx
@@ -7,12 +7,12 @@ type Props = {
 
 export default function ProjectCard(props: Props) {
   return (
-    <div className="h-full w-1/4 bg-white rounded-lg shadow-xl">
+    <div className="h-full bg-white rounded-lg shadow-xl">
         <div className="bg-gray-400 w-full h-1/3 rounded-t-md"></div>
     
         <div className="ml-1">
-            <h1 className="text-purple font-bold text-xl">{props.title}</h1>
-            <p>
+            <h1 className="text-purple font-bold text-xl select-none">{props.title}</h1>
+            <p className="select-none">
                 {props.description}
             </p>
         </div>
