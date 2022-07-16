cbc93ee59b76f72ff3aa44c49d29f0a56f29a0e0
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-07-16 15:44:20 +0100

    Added select-none to all headers

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -4,15 +4,15 @@ export default function Navbar() {
 	return (
 		<div className="flex w-full mt-1">
 			<div className="flex  flex-shrink-0 w-fit">
-				<h3 onClick={() => Router.push("/")} className="text-xl text-white font-bold ml-1 cursor-pointer">Daniel McPherson</h3>
+				<h3 onClick={() => Router.push("/")} className="text-xl text-white font-bold ml-1 select-none cursor-pointer">Daniel McPherson</h3>
 			</div>
 			<div className="flex justify-end w-full">
 				<div className="flex w-2/5 justify-evenly">
-					<h3 onClick={() => Router.push("/")} className="text-lg text-white font-semibold hover:cursor-pointer">Home</h3>
-					<h3 onClick={() => Router.push("/projects")} className="text-lg text-white font-semibold hover:cursor-pointer">Projects</h3>
-					<h3 onClick={() => Router.push("/experience")} className="text-lg text-white font-semibold hover:cursor-pointer">Experience</h3>
-					<h3 onClick={() => Router.push("/skills")} className="text-lg text-white font-semibold hover:cursor-pointer">Skills</h3>
-					<h3 onClick={() => Router.push("/contact")} className="text-lg text-white font-semibold hover:cursor-pointer">Contact</h3>
+					<h3 onClick={() => Router.push("/")} className="text-lg text-white font-semibold select-none hover:cursor-pointer">Home</h3>
+					<h3 onClick={() => Router.push("/projects")} className="text-lg text-white font-semibold select-none hover:cursor-pointer">Projects</h3>
+					<h3 onClick={() => Router.push("/experience")} className="text-lg text-white font-semibold select-none hover:cursor-pointer">Experience</h3>
+					<h3 onClick={() => Router.push("/skills")} className="text-lg text-white font-semibold select-none hover:cursor-pointer">Skills</h3>
+					<h3 onClick={() => Router.push("/contact")} className="text-lg text-white font-semibold select-none hover:cursor-pointer">Contact</h3>
 				</div>
 			</div>
 		</div>
