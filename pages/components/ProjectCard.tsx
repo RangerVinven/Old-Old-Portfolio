ca8bcabf819c7c7950ce0c605f34929cfea0e494
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-07-16 17:05:24 +0100

    Added an imageAddress to the props

--- pages/components/ProjectCard.tsx
+++ pages/components/ProjectCard.tsx
@@ -3,14 +3,15 @@ import React from 'react';
 type Props = {
     title: string,
     description: string,
-    githubLink: string
+    githubLink: string,
+	imageAddress: string
 }
 
 export default function ProjectCard(props: Props) {
   return (
     <div className="h-full bg-white rounded-lg shadow-xl">
     	<a href={props.githubLink} target="_blank" rel="noopener noreferrer">
-			<img src="/images/portfolio.png" alt="Portfolio" className="w-full h-1/3 select-none" />
+			<img src={props.imageAddress} alt="Portfolio" className="w-full h-1/3 select-none" />
     
 			<div className="ml-1">
 				<h1 className="text-purple font-bold text-xl select-none">{props.title}</h1>
