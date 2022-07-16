1ca813486ea667b27e5f91ed3803f8c720074b2c
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-07-16 01:45:47 +0100

    Added a github icon and link to my github

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
 
 import Navbar from './components/Navbar'
 import ParticlesComponent from './components/ParticlesComponent'
@@ -13,7 +14,8 @@ const Home: NextPage = () => {
 			
 			<div className="flex justify-center items-center flex-col w-full h-full">
 				<h1 className="text-6xl font-bold text-white mb-2">Hi, I'm Daniel McPherson.</h1>
-				<h1 className="text-5xl font-semibold text-white">I'm a Software Development student</h1>
+				<h1 className="text-5xl font-semibold text-white mb-2">I'm a Software Development student</h1>
+				<a href="https://github.com/RangerVinven" target="_blank" rel="noopener noreferrer"><Image src="/githubIcon.png" alt="Github Logo" height={32} width={32} /></a>
 			</div>
 		</div>
     </div>
