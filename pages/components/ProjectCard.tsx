97c394570ca98b8c3e8015c70f60747c5d0a97c8
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-07-16 16:10:16 +0100

    Added an img instead of a div to the ProjectCard

--- pages/components/ProjectCard.tsx
+++ pages/components/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 type Props = {
     title: String,
@@ -8,7 +8,7 @@ type Props = {
 export default function ProjectCard(props: Props) {
   return (
     <div className="h-full bg-white rounded-lg shadow-xl">
-        <div className="bg-gray-400 w-full h-1/3 rounded-t-md"></div>
+        <img src="/images/portfolioScreenshot.png" alt="Portfolio" className="w-full h-1/3" />
     
         <div className="ml-1">
             <h1 className="text-purple font-bold text-xl select-none">{props.title}</h1>
