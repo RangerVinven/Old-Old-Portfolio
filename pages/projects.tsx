9dbd619a17b4848e8c5245a16cff8a3a05688aff
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-07-16 15:17:07 +0100

    Added 3 ProjectsCard components

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-import Navbar from './components/Navbar'
-import PageTitle from './components/PageTitle'
+import Navbar from './components/Navbar';
+import PageTitle from './components/PageTitle';
+import ProjectCard from './components/ProjectCard';
 
 export default function projects() {
     return (
-        <div className="bg-purple min-h-screen">
+        <div className="bg-purple min-h-screen min-w-full overflow-hidden">
             <Navbar />
 
             <PageTitle title="Personal Projects" />
+
+            <div className="w-full h-3/5 flex justify-center items-center mt-16 absolute">
+                <div className="flex justify-around items-center w-4/5 h-full">
+                    <ProjectCard title="My Portfolio" description="The very website that you're on just now. I made this website to show my skills and to play around with various NPM packages" />
+                    <ProjectCard title="My Portfolio" description="The very website that you're on just now. I made this website to show my skills and to play around with various NPM packages" />
+                    <ProjectCard title="My Portfolio" description="The very website that you're on just now. I made this website to show my skills and to play around with various NPM packages" />
+                </div>
+            </div>
         </div>
     )
 }
